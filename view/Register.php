1d18db87217d2a2810c7d0c3db74b8488ae98a0e
Samio Hasan <138763106+samio11@users.noreply.github.com>
2024-04-30 11:33:15 +0600

    Search by Id,name or email Done

--- view/Register.php
+++ view/Register.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/register.css">
     <title>Register</title>
     <?php
-      include '../controller/add_customer.php';
+    include '../controller/add_customer.php';
     ?>
 </head>
+
 <body>
-<div class="login_full_body">
+    <div class="login_full_body">
         <div class="login_small_part">
             <!-- left part -->
             <div class="left_part">
@@ -21,43 +23,43 @@
                         <br>
                         <p>
                             <?php
-                                echo $nameErr;
+                            echo $nameErr;
                             ?>
                         </p>
                         <p>Email</p>
                         <input type="email" name="email" id="email1">
                         <br>
                         <?php
-                                echo $emailErr;
-                            ?>
+                        echo $emailErr;
+                        ?>
                         <p>user type</p>
                         <input type="email" name="user_type" id="user_type1" disabled>
                         <p>Phone Number</p>
                         <input type="text" name="phone" id="phone1">
                         <br>
                         <?php
-                                echo $phoneErr;
-                            ?>
+                        echo $phoneErr;
+                        ?>
                         <p>Address</p>
                         <input type="text" name="address" id="address1">
                         <br>
                         <?php
-                                echo $addressErr;
-                            ?>
+                        echo $addressErr;
+                        ?>
                         <p>Password</p>
                         <input type="password" name="password" id="password1"> <br>
                         <?php
-                                echo $passwordErr;
-                            ?>
+                        echo $passwordErr;
+                        ?>
                         <div class="center">
                             <input type="submit" class="common_btn" value="Register" name="submit">
                         </div>
                     </form>
 
                     <div class="center">
-                            <button onclick="register_login()" class="common_btn">Back LogIn</button>
-                        </div>
-                  
+                        <button onclick="register_login()" class="common_btn">Back LogIn</button>
+                    </div>
+
                 </div>
             </div>
             <!-- right Part -->
@@ -68,4 +70,5 @@
     </div>
     <script src="../controller/JS/work_btn.js"></script>
 </body>
+
 </html>
--- view/employee_dashbord.php
+++ view/employee_dashbord.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/admin_dash.css">
     <title>Document</title>
 </head>
+
+
 <body>
-<h2 class="text_center"> this is Employee dashbord</h2>
+    <h2 class="text_center"> this is Employee dashbord</h2>
     <?php
     session_start();
     if ($_SESSION['email']) {
         echo '<div class="s1">';
         echo '<div>';
-        echo '<p>'.'Welcome(Employee)' . $_SESSION['email'] . '</p>';
+        echo '<p>' . 'Welcome(Employee)' . $_SESSION['email'] . '</p>';
         echo '</div>';
         echo '<div>';
         echo "<a class='btn' href='../controller/logout.php'>Logout</a>";
@@ -41,12 +44,12 @@
 
         <tbody>
             <?php
-                include '../connection/connectiondb.php';
-                $mydb1 = new model();
-                $connobj = $mydb1->openConn();
-                $result = $mydb1->showCustomer($connobj, "customer");
-                if ($result) {
-                  while ($row = mysqli_fetch_assoc($result)) {
+            include '../connection/connectiondb.php';
+            $mydb1 = new model();
+            $connobj = $mydb1->openConn();
+            $result = $mydb1->showCustomer($connobj, "customer");
+            if ($result) {
+                while ($row = mysqli_fetch_assoc($result)) {
                     $id = $row['id'];
                     $name = $row['name'];
                     $email = $row['email'];
@@ -64,11 +67,77 @@
               <td><a class= "edit_btn" href="edit_user.php?id=' . $id . '">Edit</a></td>
               <td><a class = "delete_btn" href="../controller/delete_customer.php?id=' . $id . '">Delete</a></td>
             </tr>';
-                  }
                 }
-            
+            }
+
             ?>
         </tbody>
     </table>
+    <div class="s1">
+        <div>
+            <h2>Add New Customer</h2>
+        </div>
+        <div>
+            <a class="edit_btn" href="add_new_customer.php">Add Customer</a>
+        </div>
+    </div>
+    <div class="s1">
+        <div>
+            <h2>Search Employee</h2>
+        </div>
+        <div>
+            <!-- <a class="edit_btn" href="add_new_employee.php">Add Employee</a> -->
+            <form action="">
+                <input type="text" name="input_field" placeholder="Please Enter Name of User" id="">
+                <input type="submit" name="search_btn" value="Search">
+            </form>
+        </div>
+    </div>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Password</th>
+                <th>User Type</th>
+                <th>Phone</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $mydb = new model();
+            $conobj = $mydb->OpenConn();
+            if (isset($_REQUEST['search_btn'])) {
+                $search = $_REQUEST['input_field'];
+                $sql = "SELECT * FROM customer WHERE id = '$search' or name = '$search' or email = '$search'";
+                $result = mysqli_query($conobj, $sql);
+                if (mysqli_num_rows($result) > 0) {
+                    $row = mysqli_fetch_assoc($result);
+                    echo '<tbody>
+                    <tr>
+                    <td>' . $row['id'] . '</td>
+                    <td>' . $row['name'] . '</td>
+                    <td>' . $row['email'] . '</td>
+                    <td>' . $row['password'] . '</td>
+                    <td>' . $row['user_type'] . '</td>
+                    <td>' . $row['phone'] . '</td>
+                    </tr>
+                    </tbody>
+                    ';
+                } else {
+                    echo '<tr>
+                        <td>No Data Found</td>
+                        </tr>';
+                }
+            }
+            ?>
+
+        </tbody>
+    </table>
+
+
+
 </body>
+
 </html>
